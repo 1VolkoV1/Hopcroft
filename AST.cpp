ce06ac630aae0410e0eca6dba37c8ab918680c22
PomergranateFan <dmitry1680490@gmail.com>
2022-04-22 15:49:26 +0300

    отладочный вывод дерева и функцииции для ast->dfa

--- AST.cpp
+++ AST.cpp
@@ -122,3 +122,91 @@ AST* createStar(const char* content) {
   return node;
 }
 
+void printAST(AST* ast) {
+  std::cout << "type: " << ast->type;
+  if (ast->type == leaf_node) std::cout << " value: " << ast->value;
+  std::cout << " fpos: ";
+  for (int i = 0; i < ast->fpos.size(); i++) std::cout << ast->fpos[i] << " ";
+  std::cout << " lpos: ";
+  for (int i = 0; i < ast->lpos.size(); i++) std::cout << ast->lpos[i] << " ";
+  std::cout << std::endl;
+  if (ast->type == cat_node || ast->type == or_node) {
+    printAST(ast->left);
+    printAST(ast->right);
+  }
+}
+
+std::vector<int> alphabet(AST* ast) {
+  if (ast->type == leaf_node) {
+    std::vector<int> ret { ast->value };
+    return ret;
+  }
+
+  if (ast->type == cat_node || ast->type == or_node)
+    return concat(
+      alphabet(ast->left),
+      alphabet(ast->right)
+    );
+  if (ast->type == star_node) return alphabet(ast->left);
+
+  return {};
+}
+
+void putinmap(std::vector<int> *map, AST* ast) {
+  switch(ast->type) {
+    case leaf_node:
+      (*map)[ast->fpos[0]] = ast->value;
+      break;
+    case cat_node:
+    case or_node:
+      putinmap(map, ast->right);
+    case star_node:
+      putinmap(map, ast->left);
+  }
+}
+
+std::vector<int> CharValueMap(AST* ast) {
+  std::vector<int> map;
+  map.resize(ast->lpos[0]);
+  putinmap(&map, ast);
+  return map;
+}
+
+void computeFpos(
+  std::vector<std::vector<int>> *followpos, AST* ast
+) {
+  auto left = ast->left;
+  auto right = ast->right;
+  if (ast->type == cat_node) {
+    for (int i = 0; i < left->lpos.size(); i++) {
+      int j = left->lpos[i];
+      (*followpos)[j] = concat(
+        (*followpos)[j], right->fpos
+      );
+    }
+    computeFpos(followpos, left);
+    computeFpos(followpos, right);
+  }
+  if (ast->type == or_node) {
+    computeFpos(followpos, left);
+    computeFpos(followpos, right);
+  }
+  if (ast->type == star_node) {
+    for (int i = 0; i < left->lpos.size(); i++) {
+      int j = left->lpos[i];
+      (*followpos)[j] = concat(
+        (*followpos)[j], left->fpos
+      );
+    }
+    computeFpos(followpos, left);
+  }
+  return;
+}
+
+std::vector<std::vector<int>> FollowPos(AST* ast) {
+  std::vector<std::vector<int>> fpos;
+  fpos.resize(ast->lpos[0]);
+  for (int i = 0; i < fpos.size(); i++) fpos[i] = {};
+  computeFpos(&fpos, ast);
+  return fpos;
+}
